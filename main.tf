d37f3312a4dd140c9ab70de4fae61975f7f14261
kanaveni25 <113719659+kanaveni25@users.noreply.github.com>
2023-02-04 09:36:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
  resource "aws_instance" "example" {
    ami           = "ami-04893cdb768d0f9ee"
    instance_type = "t2.micro"
-   key_name = "aplb"
-
+   key_name = "maheshaws"
 
  }
